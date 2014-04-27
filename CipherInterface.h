579039757b1c2bac64268faceecb858d0df2c6c7
kevincheng99 <kevincheng99@csu.fullerton.edu>
2014-04-26 23:07:17 -0700

    update cipher interface

--- CipherInterface.h
+++ CipherInterface.h
@@ -19,13 +19,24 @@ class CipherInterface
 		 */
 		CipherInterface(){}
 		
-		
 		/**
 		 * Sets the key to use
 		 * @param key - the key to use
 		 * @return - True if the key is valid and False otherwise
 		 */
 		virtual bool setKey(const string& key){ return false;  }
+
+    /**
+     * Sets the RSA key to use
+     * The RSA key can be either the puclic key or private key in PEM format.
+     *
+     * @param key_file - the key file
+     * @param is_public_key - True if the given key file contains the public key
+     * in PEM forma. False if the given file contains the private key in PEM
+     * format.
+     * @return - True if the key is successfully read or False, otherwise.
+     */
+		virtual bool setKey(const string& key_file, bool is_public_key){ return false;  }
 
 		/**	
 		 * Encrypts a plaintext string
